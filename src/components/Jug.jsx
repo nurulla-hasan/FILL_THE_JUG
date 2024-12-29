7bd6c20ad6ea760d1c2f4eae998ab25a7ca404b5
Nurulla Hasan <nurullahasan.dev@gmail.com>
2024-12-29 20:23:23 +0600

    wave animate added for 3s when fillby value change

--- src/components/Jug.jsx
+++ src/components/Jug.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Jug = ({ fillBy }) => {
+
+    const [levelChange, setLevelChange] = useState(false);
+
+
+    useEffect(() => {
+        setLevelChange(true)
+
+        const timeOutId = setTimeout(() => {
+            setLevelChange(false)
+        }, 3000);
+
+        return () => {
+            clearTimeout(timeOutId);
+        }
+    }, [fillBy])
+
+
     return (
         <div className='w-[300px] h-[450px] relative border-black border-t-0 border-2 rounded-b-3xl overflow-hidden'>
-            <div style={{ height: `${fillBy}%` }} className='w-full absolute bottom-0 bg-blue-500 rounded-b-3xl animate-wave '>
+            <div className={`w-full absolute bottom-0 bg-blue-500 rounded-b-3xl ${levelChange ? 'animate-wave' : ''}`} style={{ height: `${fillBy}%` }}>
                 <span className='text-white font-bold text-xl animate-pulse'>{Math.round(fillBy)}%</span>
             </div>
         </div>
